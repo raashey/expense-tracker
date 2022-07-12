1b9681edd3be64e67129f2ea4c7e05e0ef7c8fe2
Radosław Auguścik <raashey@MacBook-Air-Radosaw.local>
2022-07-12 18:46:34 +0200

    added states to item

--- src/expense-item/ExpenseItem.js
+++ src/expense-item/ExpenseItem.js
@@ -1,24 +1,67 @@
+import { useState } from "react";
 import { Card } from "react-bootstrap";
 import { ExpenseDate } from "./ExpenseDate";
 import "./ExpenseItem.scss";
 
 export const ExpenseItem = (props) => {
+  const [clicked, setClicked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
+  const notHoveredStyle = "expense-item bg-secondary m-3";
+  const hoveredStyle = "expense-item bg-secondary m-3 border border-warning";
+
+  const setClickedState = () => {
+    setClicked(!clicked);
+  };
+
+  const setHoveredStateTrue = () => {
+    setHovered(true);
+  };
+
+  const setHoveredStateFalse = () => {
+    setHovered(false);
+  };
+
   const date = new Date(props.date);
-  return (
-    <Card className="expense-item bg-secondary m-3">
-      <Card.Body className="d-flex flex-row justify-content-between align-items-center text-light">
-        <div className="d-flex grow-2 flex-row justify-content-start">
-        <ExpenseDate date={date} className="d-flex grow-2 flex-row justify-content-center"></ExpenseDate>
-        </div>
-        <div className="d-flex grow-6 flex-row justify-content-center">
-          <h2>{props.name}</h2>
-        </div>
-        <div className="d-flex grow-2 flex-row justify-content-end">
-            <div className="price d-flex justify-content-center align-items-center border border-warning">
-          <h2>{props.price}</h2>
+
+  if (!clicked) {
+    return (
+      <Card
+        className={hovered ? hoveredStyle : notHoveredStyle}
+        onClick={setClickedState}
+        onMouseOver={setHoveredStateTrue}
+        onMouseOut={setHoveredStateFalse}
+      >
+        <Card.Body className="d-flex flex-row justify-content-between align-items-center text-light">
+          <div className="d-flex grow-2 flex-row justify-content-start">
+            <ExpenseDate
+              date={date}
+              className="d-flex grow-2 flex-row justify-content-center"
+            ></ExpenseDate>
           </div>
-        </div>
-      </Card.Body>
-    </Card>
-  );
+          <div className="d-flex grow-6 flex-row justify-content-center">
+            <h2>{props.name}</h2>
+          </div>
+          <div className="d-flex grow-2 flex-row justify-content-end">
+            <div className="price d-flex justify-content-center align-items-center border border-warning">
+              <h2>{props.price}</h2>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    );
+  } else {
+    return (
+      <Card
+        className={hovered ? hoveredStyle : notHoveredStyle}
+        onClick={setClickedState}
+        onMouseOver={setHoveredStateTrue}
+        onMouseOut={setHoveredStateFalse}
+      >
+        <Card.Body className="d-flex flex-row justify-content-between align-items-center text-light">
+          Uno reverse
+        </Card.Body>
+      </Card>
+    );
+  }
 };
--- src/expense-list/ExpenseList.js
+++ src/expense-list/ExpenseList.js
@@ -4,10 +4,10 @@ import './ExpenseList.scss'
 
 export const ExpenseList = () => {
   const expenses = [
-    { date: "2022-07-02", name: "Groceries", price: "20$" },
-    { date: "2022-07-04", name: "Zara", price: "50$" },
-    { date: "2022-07-07", name: "Douglas", price: "100$" },
-    { date: "2022-07-10", name: "McDonalds", price: "15$" },
+    { id: 1, date: "2022-07-02", name: "Groceries", price: "20$" },
+    { id: 2, date: "2022-07-04", name: "Zara", price: "50$" },
+    { id: 3, date: "2022-07-07", name: "Douglas", price: "100$" },
+    { id: 4, date: "2022-07-10", name: "McDonalds", price: "15$" },
   ];
 
   return (
@@ -16,7 +16,8 @@ export const ExpenseList = () => {
         <Card.Title className="text-light">List of expenses</Card.Title>
         {
           expenses.map(item => {
-            return <ExpenseItem 
+            return <ExpenseItem
+                    key={item.id} 
                     date={item.date} 
                     name={item.name}
                     price={item.price}/>
